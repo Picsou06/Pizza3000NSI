232fd22535d2117a31e05d7bebb61591a23998d3
Picsou06 <evan.ossamlad@gmail.com>
2022-12-07 00:13:46 +0100

    creationdepizza

--- creationpizza.py
+++ creationpizza.py
@@ -1,25 +1,29 @@
-lp=open("pizzafav.txt", "w")
+lp=open("ingredients PIZZA.txt", "r")
+Lines = lp.readlines()
+txtinput=[]
+for line in Lines:
+    txtinput.append(line)
 print("Quel taille souhaitez vous pour votre pizza?")
-txtinput=lp.readline(1)
 taille=0
 pizza=[]
-while taille>=1 and taille<=3:
-    taille=input(txtinput)
+while taille<1 or taille>3:
+    taille=int(input(txtinput[0]))
 pizza.append(taille)
 print("Quel base souhaitez vous?")
-txtinput=lp.readline(3)
 base=0
-while base>=1 and base<=4:
-    base=input(txtinput)
+while base<1 or base>4:
+    base=int(input(txtinput[1]))
 pizza.append(base)
 print("Quel fromages souhaitez vous?")
-txtinput=lp.readline(5)
 fromage=0
-while fromage>=1 and fromage<=4:
-    fromage=input(txtinput)
+while fromage<1 or fromage>4:
+    fromage=int(input(txtinput[2]))
 pizza.append(fromage)
 print(pizza)
-taille={"29cm","33cm","40cm"}
-base={"Sauce Tomates","Creme fraiche","Huile d'olives","Aucune"}
-fromage={"Gruyere","Mozarella","Emmantal"}
-print("Vous avez donc choisi une pizza de "+taille[base[0]-1]+" avec une base "+taille[base[1]-1]+" et avec "+taille[base[2]-1]+" comme fromage")
+taille=["29cm","33cm","40cm"]
+base=["Sauce Tomates","Creme fraiche","Huile d'olives","Aucune"]
+fromage=["Gruyere","Mozarella","Emmantal"]
+print("Vous avez donc choisi une pizza de "+taille[pizza[0]-1]+" avec une base "+base[pizza[1]-1]+" et avec "+fromage[pizza[2]-1]+" comme fromage")
+addfav=input("Souhaitez vous l'ajouter à vos pizzas favoris?")
+lp=open("pizzafav.txt", "a")
+lp.write()
