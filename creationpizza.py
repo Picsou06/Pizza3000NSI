f3ebfcedd054740b48b25839be0a9dac14efc074
Picsou06 <evan.ossamlad@gmail.com>
2022-12-06 23:39:28 +0100

    Mise a jour

--- creationpizza.py
+++ creationpizza.py
@@ -19,3 +19,7 @@ while fromage>=1 and fromage<=4:
     fromage=input(txtinput)
 pizza.append(fromage)
 print(pizza)
+taille={"29cm","33cm","40cm"}
+base={"Sauce Tomates","Creme fraiche","Huile d'olives","Aucune"}
+fromage={"Gruyere","Mozarella","Emmantal"}
+print("Vous avez donc choisi une pizza de "+taille[base[0]-1]+" avec une base "+taille[base[1]-1]+" et avec "+taille[base[2]-1]+" comme fromage")
