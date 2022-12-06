81b63bceee5e3d93afaa1c3704f1ac4c258c64c7
Picsou06 <110305876+Picsou06@users.noreply.github.com>
2022-12-06 17:27:26 +0100

    test

--- choixdepizza.py
+++ choixdepizza.py
@@ -9,7 +9,7 @@ print("Merci de votre commande de:")
 print(z[choix-1])
 choix=input("Souhaitez vous commander autre chose? 0: Non, 1: Oui")
 if choix==0:
-    x=0
+    stop=0
     print("Au revoir!")
 elif choix==1:
     exec(open("./start.py").read())
