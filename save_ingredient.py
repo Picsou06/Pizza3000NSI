07478a45765b424afa8559d8df8030fd84448fdc
Picsou06 <110305876+Picsou06@users.noreply.github.com>
2022-12-13 12:21:44 +0100

    save ingredient mise a jour

--- save_ingredient.py
+++ save_ingredient.py
@@ -1,18 +1,29 @@
 lp=open("ingredients PIZZA.txt", "r")
-vi=open("variableingredients.txt", "a")
+vi=open("variableingredients.txt", "w")
 Lines = lp.readlines()
 txtinput=[]
 nom=[]
 autre=[]
 ingredient=[]
+ingredientt=[]
 for line in Lines:
     txtinput.append(line)
 for i in txtinput:
     temp=i.split(" = ")
     nom.append(temp[0])
     autre.append(temp[1])
-for i in ingredient:
+for i in autre:
     temp=i.split(" ")
+    ingredientt.append(temp)
+for i in ingredientt:
+    temp=[]
+    for j in range(len(i)):
+        if j%2!=0:
+            temp.append(i[j])
     ingredient.append(temp)
-for i in range(nom):
-    vi.write(nom[i]+"="+ingredient[i])
+for i in range(len(nom)):
+    ch=str(nom[i])+"="+str(ingredient[i])+"\n"
+    ch.rstrip()
+    ch.encode('utf-8')
+    vi.write(ch)
+vi.close()
