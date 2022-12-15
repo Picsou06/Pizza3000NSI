2aa249521d55d29c99edb747b8fb21432da49e86
Picsou06 <evan.ossamlad@gmail.com>
2022-12-15 18:52:35 +0100

    mise a jour

--- creationpizza.py
+++ creationpizza.py
@@ -24,17 +24,13 @@ print("Quel supplement souhaitez vous?")
 supplement=0
 while supplement<1 or supplement>8:
     supplement=int(input(txtinput[3]))
+pizza.append(supplement)
 print("Quel supplement souhaitez vous?")
-huile=0
-while huile<1 or huile>2:
-    huile=int(input(txtinput[4]))
-pizza.append(huile)
 Taille=['29cm', '33cm', '40cm']
 Base=['Sauce_Tomates', 'Creme_fraiche', "Huile_d'olives", 'Aucune']
-Fromages=['Gruyere', 'Mozarella', 'Emmantal', 'Cantal', 'Gorgonzola', 'Cheddar', 'Conte', 'Chevre', 'Roquefort', 'Roblechon', 'Aucun']
-Supplement=['Steak_Hache', 'Saumon', 'Olives', 'Champignon', 'Basilic', 'Anchois', 'Frittes', 'Aucun']
-Huile=['Huile_Piquante', 'Aucun']
-print("Vous avez donc choisi une pizza de "+taille[pizza[0]-1]+" avec une base "+base[pizza[1]-1]+" et comme ingredients:"+fromage[pizza[2]-1])
+Fromages=['Gruyere', 'Mozarella', 'Emmental', 'Cantal', 'Gorgonzola', 'Cheddar', 'Conte', 'Chevre', 'Roquefort', 'Roblechon', 'Aucun']
+Supplement=['Steak_Hache', 'Saumon', 'Olives', 'Champignon', 'Basilic', 'Anchois', 'Frites']
+print("Vous avez donc choisi une pizza de "+taille[pizza[0]-1]+" avec une base "+base[pizza[1]-1]+" et comme ingredients:"+Fromages[pizza[2]-1]+", "+Supplement[pizza[3]-1])
 addfav=input("Souhaitez vous l'ajouter a vos pizzas favoris?")
 if addfav=="1":
     lpa=open("pizzafav.txt", "a")
@@ -46,7 +42,8 @@ if addfav=="1":
         x=x+1
     x=x/5
     x=int(x+1)
-    lpa.write("\n"+str(x)+":"+"\n"+"\t"+"Nom="+nom+"\n"+"\t"+"Diametre="+taille[pizza[0]-1]+"\n"+"\t"+"base="+base[pizza[1]-1]+"\n"+"\t"+"ingredient="+fromage[pizza[2]-1])
+    lpa.write("\n"+str(x)+":"+"\n"+"\t"+"Nom="+nom+"\n"+"\t"+"Diametre="+taille[pizza[0]-1]+"\n"+"\t"+"base="+base[pizza[1]-1]+"\n"+"\t"+"ingredient=")
+    lpa.write(Fromages[pizza[2]-1]+","+Supplement[pizza[3]-1])
     print("Pizza ajoute a vos favoris!")
 else:
     print("Au revoir, merci de votre visite!")
