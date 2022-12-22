c19bdaf5f21b8809b05f7b37fed94f2c53c0083b
Pr3Dat3UR23 <120458546+Pr3Dat3UR23@users.noreply.github.com>
2022-12-22 16:09:04 +0100

    Update choixdepizza.py

--- choixdepizza.py
+++ choixdepizza.py
@@ -1,23 +1,23 @@
 from time import sleep # importation de sleep
-lp=open("pizzafav.txt", "r") #Creation d'une varible lp qui est egale à
+lp=open("pizzafav.txt", "r") #Creation d'une varible lp
 p=lp.read() #Creation d'une varible p qui est egale
 print(p)  #Affichage du contenu de la variable p
-lp=open("pizzafav.txt", "r")
-Lines = lp.readlines()
-pizza=[]
-x=0
-for line in Lines:
-    x=x+1
-    pizza.append(line)
-x=x/5
-choix=0
-while choix>x or int(choix)<1:
-    choix=int(input("Quel pizza souhaitez vous commander?"))
-print("Merci de votre commande de:")
-l=0
-l=(choix-1)*5
-for i in range(4):
-    l=l+1
-    print(pizza[l])
-sleep(3)
+lp=open("pizzafav.txt", "r")#Mise a jour de la variable lp
+Lines = lp.readlines()#Creation de la variable Lines
+pizza=[]#Creation de la variabe pizza
+x=0#Mise à jour de la varible x
+for line in Lines:#Repeter Lines fois
+    x=x+1#Mise a jour de la variable x
+    pizza.append(line)#Ajout dans la variable pizza de la variable line
+x=x/5#Mise a jour de la variable x
+choix=0#Creation de la variabe choix
+while choix>x or int(choix)<1:#Tant que choix > x ou choix < 1
+    choix=int(input("Quel pizza souhaitez vous commander?"))#Mise a jour de la variable choix
+print("Merci de votre commande de:")#Affichage de "Merci de votre commande de"
+l=0#Creation de la variabe l
+l=(choix-1)*5#Mise a jour de la variable l
+for i in range(4):#Repeter 4 fois
+    l=l+1#Mise a jour de la variable l
+    print(pizza[l])#Afficher le 2eme element de la variable pizza
+sleep(3)#Attendre 3 secondes
 
