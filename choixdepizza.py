f81c7fd090279d9e4715e0ecad3dd742ce67e70e
Pr3Dat3UR23 <120458546+Pr3Dat3UR23@users.noreply.github.com>
2022-12-29 17:01:25 +0100

    Update choixdepizza.py

--- choixdepizza.py
+++ choixdepizza.py
@@ -1,12 +1,12 @@
-from main import money
+from main import money # importation de money
 from time import sleep # importation de sleep
-lp=open("pizzafav.txt", "r") #Creation d'une varible lp
+lp=open("pizzafav.txt", "r") #Creation d'une variable lp
 p=lp.read() #Creation d'une varible p qui est egale
 print(p)  #Affichage du contenu de la variable p
 lp=open("pizzafav.txt", "r")#Mise a jour de la variable lp
 Lines = lp.readlines()#Creation de la variable Lines
 pizza=[]#Creation de la variabe pizza
-x=0#Mise à jour de la varible x
+x=0#Mise à jour de la variable x
 for line in Lines:#Repeter Lines fois
     x=x+1#Mise a jour de la variable x
     pizza.append(line)#Ajout dans la variable pizza de la variable line
@@ -16,17 +16,17 @@ while choix>x or int(choix)<1:#Tant que choix > x ou choix < 1
     choix=int(input("Quel pizza souhaitez vous commander?"))#Mise a jour de la variable choix
 #recupération du prix:
 l=(choix-1)*6+5#Mise a jour de la variable l
-prix=pizza[l]
-prix=prix.split("=")
-prix=int(prix[1])
-if prix<money:
-    money=money-prix
+prix=pizza[l]#Mise à jour de la variable prix
+prix=prix.split("=")#Mise à jour de la varible prix
+prix=int(prix[1])#Mise à jour de la varible prix
+if prix<money:#Si prix<money
+    money=money-prix#Mise à jour de la variable 
     print("Merci de votre commande de:")#Affichage de "Merci de votre commande de"
     l=(choix-1)*6#Mise a jour de la variable l
     for i in range(5):#Repeter 4 fois
         l=l+1#Mise a jour de la variable l
         print(pizza[l])#Afficher le 2eme element de la variable pizza
     sleep(2)#Attendre 3 secondes
-else:
-    print("Vous n'avez pas assez d'argent pour acheter cette pizza!")
+else:#Sinon
+    print("Vous n'avez pas assez d'argent pour acheter cette pizza!")#Affichage de "Vous n'avez pas assez d'argent pour acheter cette pizza!"
 
