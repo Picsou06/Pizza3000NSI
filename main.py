4cd2abd2a114b2497642f7a6a27a4837dc6a0d3b
Picsou06 <110305876+Picsou06@users.noreply.github.com>
2023-01-03 12:39:41 +0100

    voilà

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ print("Vous possedez actuellement "+str(money)+nomdelamoney)
 stop = 1 #Creation d'une varible stop pour la boucle
 
 while stop==1: #Tant que Stop est egal à 1 :
-    if money>=0:
+    if money>=0:#Si money>=0
         x= open("start.py", "r") #Creation de la variable x pour lire le fichier start.py
-        exec(x.read()) #Lecture de la variable x donc lecture du fichier start.py
-    else:
-        print("Vous devez travailler pour gagner de l'argent!")
+        exec(x.read()) #Lecture de start.py
+    else:#Sinon
+        print("Vous devez travailler pour gagner de l'argent!")#Affichage de "Vous devez travailler pour gagner de l'argent!"
         exec(open("./creationpizza.py").read())
