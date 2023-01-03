bbc4d0c78009027718e60564bde074802d1597e2
Pr3Dat3UR23 <120458546+Pr3Dat3UR23@users.noreply.github.com>
2023-01-03 11:53:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 print("Chargement de Pizza3000...") #Affichage de Chargement de Pizza3000... (déco)
+
 money=50
 nomdelamoney=" Euros"
 print("Vous possedez actuellement "+str(money)+nomdelamoney)
 
 stop = 1 #Creation d'une varible stop pour la boucle
+
 while stop==1: #Tant que Stop est egal à 1 :
     if money>=0:
         x= open("start.py", "r") #Creation de la variable x pour lire le fichier start.py
